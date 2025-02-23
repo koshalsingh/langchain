6d274343488447f34ef24ae1af7b1155936030bb
Koshal kumar <koshalsingh0921@gmail.com>
2025-02-23 11:24:22 +0530

    deleted useless codes.

--- chatbot/locallama.py
+++ chatbot/locallama.py
@@ -2,13 +2,15 @@ import streamlit as st
 import os
 from dotenv import load_dotenv
 from langchain.prompts import ChatPromptTemplate
-from langchain.llms import HuggingFaceHub, Ollama
+from langchain.llms import Ollama
 
 # Load environment variables
 load_dotenv()
 
 # Set API key for Hugging Face
 HUGGINGFACE_API_KEY = os.getenv("HUGGINGFACEHUB_API_TOKEN", "")
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY", "")
+os.environ["LANGCHAIN_TRACING_V2"] = "true"  # For LangSmith tracing
 
 # deepseek-r1:1.5b
 
