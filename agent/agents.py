0c2a7aea12b35b87764f455c932c25799d5e1c03
Koshal kumar <koshalsingh0921@gmail.com>
2025-03-12 23:19:32 +0530

    Final bug fixed in agent code

--- agent/agents.py
+++ agent/agents.py
@@ -5,9 +5,7 @@ from langchain_community.utilities import WikipediaAPIWrapper
 # %%
 api_wrapper = WikipediaAPIWrapper(top_k_results=1, doc_content_chars_max=500)
 tool_wiki = WikipediaQueryRun(api_wrapper=api_wrapper)
-
-# %%
-tool_wiki.name
+# tool_wiki.name
 
 # %%
 from langchain_community.document_loaders import WebBaseLoader
@@ -25,14 +23,14 @@ documents = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200).s
 vectordb = FAISS.from_documents(documents, embeddings)
 
 retriver = vectordb.as_retriever()
-retriver
+# retriver
 
 # %%
 from langchain.tools.retriever import create_retriever_tool
 tool_lc = create_retriever_tool(retriver,"langsmith_search",
                       "Search for information about LangSmith. For any questions about LangSmith, you must use this tool!")
 
-tool_lc.name
+# tool_lc.name
 
 # %%
 from langchain_community.utilities import ArxivAPIWrapper
@@ -40,37 +38,47 @@ from langchain_community.tools import ArxivQueryRun
 
 arxiv_wrapper = ArxivAPIWrapper(top_k_results=1, doc_content_chars_max=500)
 tool_arxiv = ArxivQueryRun(api_wrapper=arxiv_wrapper)
-tool_arxiv.name
+# tool_arxiv.name
 
-# %%
 tools = [tool_wiki, tool_lc, tool_arxiv]
 tools
 
-# %%
-from langchain_community.llms import Ollama
+from langchain_groq import ChatGroq
 
-r1_model = Ollama(model = "deepseek-r1:14b")
-r15_model = Ollama(model = "deepseek-r1:1.5b")
+# Initialize the language model of Groq
+llm = ChatGroq(
+    model_name="llama3-70b-8192",
+    temperature=0.3,
+    max_tokens=None,
+    timeout=None,
+    max_retries=2,
+)
 
-# %%
+# Bind tools to the language model
+llm_with_tools = llm.bind_tools(tools)
+
+print(hasattr(llm_with_tools, "bind_tools"))
+
 from langchain import hub
 
 prompt = hub.pull("hwchase17/openai-functions-agent")
-prompt.messages
+print(prompt.messages)
 
-# %%
-## Agents
-from langchain.agents import create_openai_tools_agent
+from langchain.agents import AgentExecutor, create_tool_calling_agent, tool
 
-agent = create_openai_tools_agent(r15_model, tools, prompt)
-agent
+# Bind tools to the language model
+llm_with_tools = llm.bind_tools(tools)
 
-# %%
-# agent executor
+# Create the agent
+agent = create_tool_calling_agent(llm_with_tools, tools, prompt)
+
+from langchain.agents import AgentExecutor
+# Create an agent executor
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
 from langchain.agents import AgentExecutor, AgentType, initialize_agent, load_tools
 
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-agent_executor
-
-# %%
-agent_executor({"input": "Tell me about LangSmith"})
+# agent_executor
+agent_executor.invoke({"input":"Tell me about LangSmith"})
+agent_executor.invoke({"input":"What's the paper 1605.08386 about?"})
